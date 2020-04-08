dc36be3d65228457814b6c6cba0f272f672a645b
Lucas <lucasdammel@gmail.com>
2020-04-08 15:01:11 +0200

    Timer + Score Display

---
+++ MonsterWG/Assets/Scripts/Game/UI/ScoreDisplay.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Game.UI
+{
+    public class ScoreDisplay : MonoBehaviour
+    {
+        [SerializeField] private int score;
+        [SerializeField] private TextMeshProUGUI scoreText;
+        [SerializeField] private GameObject[] fullStars = new GameObject[5];
+        [SerializeField] private int[] neededStarScore = new int[5];
+
+        private void OnEnable()
+        {
+            foreach (var star in fullStars)
+            {
+                star.SetActive(false);
+            }
+
+            CalculateScore();
+        }
+
+        private void CalculateScore()
+        {
+            //DoCalculation
+            DisplayScore();
+        }
+
+        private void DisplayScore()
+        {
+            scoreText.text = score.ToString();
+            for (int i = 0; i < neededStarScore.Length; i++)
+            {
+                if (score >= neededStarScore[i])
+                {
+                    fullStars[i].SetActive(true);
+                }
+                else return;
+            }
+        }
+    }
+}
---
+++ MonsterWG/Assets/Scripts/Game/Utility/CountdownTimer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.Utility
+{
+    public class CountdownTimer : MonoBehaviour
+    {
+        #region Variables
+        
+        public float TimeLeft => timeLeft;
+        
+        [SerializeField] private float timeLeft = 300.0f;
+        [SerializeField] private TextMeshProUGUI text;
+        [SerializeField] private Image timerImage;
+        [SerializeField] private ToggleObject target;
+ 
+        private bool _stop = true;
+        private float _minutes;
+        private float _seconds;
+        private float _startTime;
+        
+        #endregion
+
+        #region Functions
+        
+        private void Start()
+        {
+            StartTimer(timeLeft);
+        }
+
+        public void StartTimer(float from)
+        {
+            _stop = false;
+            timeLeft = from;
+            _startTime = timeLeft;
+            StartCoroutine(UpdateCoroutine());
+        }
+
+        private void Update() 
+        {
+            if(_stop) return;
+            timeLeft -= Time.deltaTime;
+         
+            _minutes = Mathf.Floor(timeLeft / 60);
+            _seconds = timeLeft % 60;
+            if(_seconds > 59) _seconds = 59;
+            if (!(_minutes < 0)) return;
+            _stop = true;
+            _minutes = 0;
+            _seconds = 0;
+            target.ToggleObjects();
+        }
+ 
+        private IEnumerator UpdateCoroutine()
+        {
+            while(!_stop)
+            {
+                text.text = $"{_minutes:0}:{_seconds:00}";
+                timerImage.fillAmount = timeLeft / _startTime;
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+        
+        #endregion
+    }
+}
