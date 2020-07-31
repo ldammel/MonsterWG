e91daa59e55350b6889bf5728f478f627e67ad03
Ajamajan <lennard.hiller@web.de>
2020-07-31 13:58:26 +0200

    Missing stuff repush

---
+++ MonsterWG/Assets/Scripts/Monster WG/Wall.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+
+[Serializable]
+public struct Wall
+{
+   public string name;
+   public GameObject wall;
+
+   public void Show()
+   {
+      wall.SetActive(true);
+   }
+
+   public void Hide()
+   {
+      wall.SetActive(false);
+   }
+}
+
