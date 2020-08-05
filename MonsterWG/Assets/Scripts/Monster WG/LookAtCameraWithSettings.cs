1a76951889d656632720ba19f4e4dd1cc12e81d3
Ajamajan <lennard.hiller@web.de>
2020-08-05 22:47:56 +0200

    Ui stuff and prefabs. imported new roy mesh

---
+++ MonsterWG/Assets/Scripts/Monster WG/LookAtCameraWithSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCameraWithSettings : MonoBehaviour
+{
+    public Transform camTransform;
+    public bool usePosition = false;
+    public Vector3 rotationOffset;
+    void Awake()
+    {
+        if(!camTransform)
+            camTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(camTransform)
+        {
+            if(usePosition)
+            {
+                transform.LookAt(camTransform.position);
+                transform.Rotate(rotationOffset + new Vector3 (0,180f,0));
+            }
+            else
+            {
+                transform.rotation = camTransform.rotation;
+                transform.Rotate(rotationOffset);
+            }
+        }
+    }
+}
