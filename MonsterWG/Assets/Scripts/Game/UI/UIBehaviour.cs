8a91c7d90579b8646d55845517530bda013d83c3
Lucas <lucasdammel@gmail.com>
2020-08-05 22:54:54 +0200

    main menu

--- MonsterWG/Assets/Scripts/Game/UI/UIBehaviour.cs
+++ MonsterWG/Assets/Scripts/Game/UI/UIBehaviour.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 namespace Game.UI
 {
-
-    public class UIBehaviour : MonoBehaviour
+    public class UIBehaviour : MonoBehaviour , IPointerEnterHandler , ISelectHandler 
     {
         public UnityEvent onActivate;
         public void Execute()
         {
             onActivate.Invoke();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Execute();
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            Execute();
         }
     }
 }
